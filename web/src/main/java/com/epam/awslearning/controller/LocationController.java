b2690554e9cb2f73b06677e2abfe20b3d2ee5b93
explosion204 <dzmitriy20magic@gmail.com>
2022-04-21 21:22:12 +0300

    ec2: fix LocationDto class

--- web/src/main/java/com/epam/awslearning/controller/LocationController.java
+++ web/src/main/java/com/epam/awslearning/controller/LocationController.java
@@ -3,6 +3,7 @@ package com.epam.awslearning.controller;
 import com.epam.awslearning.dto.LocationDto;
 import com.epam.awslearning.service.LocationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
